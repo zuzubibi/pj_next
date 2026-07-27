da451697e58757efda53cf1c0a6f615b874e5ead
zuzubibi <lsb9938@naver.com>
2026-07-27 22:29:03 +0900

    Feat: intercepting route

---
+++ pj_next/app/feed/(..)posts/[postId]/page.tsx
@@ -0,0 +1,16 @@
+interface PageProps {
+    params: Promise<{ postId: string }>;
+}
+
+async function PostItemPage(props: PageProps) {
+    const { params } = props;
+    const { postId } = await params;
+
+    return (
+        <div className="box page">
+            <p>{`Intercepted PostItemPage ${postId}`}</p>
+        </div>
+    );
+}
+
+export default PostItemPage;
