59b3a68c0a2969d207d53b7b4e1d39a0bb7f53da
zuzubibi <lsb9938@naver.com>
2026-08-12 21:39:53 +0900

    feat: 서버에서 데이터 받아오기

--- pj_next/app/feed/page.tsx
+++ pj_next/app/feed/page.tsx
@@ -1,17 +1,30 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 function FeedPage() {
+    const [posts, setPosts] = useState<MyNextApp.Post[]>([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const data = await fetch('/api/posts').then((res) => res.json());
+            setPosts(data);
+        }
+
+        fetchPosts();
+    }, []);
+    console.log("무야호", posts);
+
     return (
         <div className='box page'>
             <h4>Page</h4>
             <ul style={{ listStylePosition: 'inside' }}>
-                {[...new Array(10)].map((item, idx) => {
+                {posts.map((post, idx) => {
                     const postId = idx + 1;
                     return (
                         <li key={idx}>
-                            <Link href={`/posts/${postId}`}>
-                                {`Post item ${postId}`}
-                            </Link>
+                            <Link href={`/posts/${postId}`}>{ post.title }</Link>
                         </li>
                     );
                 })}
---
+++ pj_next/lib/types/my-next-app.d.ts
@@ -0,0 +1,8 @@
+declare namespace MyNextApp {
+    type Post = {
+        id: string;
+        title: string;
+        content: string;
+        image: string;
+    }
+}
