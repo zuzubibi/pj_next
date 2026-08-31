0cea6d1eefbae3197952acdb7cd08b5cc92ced06
zuzubibi <lsb9938@naver.com>
2026-08-31 22:33:12 +0900

    폰트 최적화 적용하기

--- pj_next/app/layout.tsx
+++ pj_next/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
 import Link from 'next/link';
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -30,7 +26,7 @@ export default function RootLayout(props: Readonly<RootLayoutProps>) {
   return (
     <html lang="en" >
       <body
-        className={`max-w-[480px] m-auto ${geistSans.variable} ${geistMono.variable}`}>
+        className={`max-w-[480px] m-auto ${notoSansKR.className}`}>
         <nav className='py-4 flex gap-4'>
           <Link href='/'>Home</Link>
           <Link href='/feed'>Feed</Link>
